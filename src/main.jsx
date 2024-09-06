4fbc5ce977be5990ee2a0ed6114e6ec2818c6136
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-09-06 09:58:41 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App/App.jsx";
+import App from "./App";
 import "modern-normalize";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
